18f91536d3d52a869ed2927d28f84ea8eb676420
Ytalo-Alves <ytaloalves@hotmail.com>
2024-09-24 16:33:11 -0300

    feat: Results in object [0]

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
 import { Dialog } from './components/ui/dialog'
 import './index.css'
-
-// import { EmptyGoals } from '../src/components/empty-goals'
 import { CreateGoal } from './components/create-goal'
+import { useEffect, useState } from 'react'
 import { Summary } from './components/summary'
+import { EmptyGoals } from './components/empty-goals'
+
 
 export function App() {
+
+  const [summary, setSummary] = useState(null)
+
+
+  useEffect(() => {
+    fetch('http://localhost:3333/summary')
+      .then(response => {
+        return response.json()
+      }).then(data => {
+        setSummary(data.summary)
+      })
+  }, [])
+
+
+
   return (
     <Dialog>
-       {/* <EmptyGoals/> */}
-       <Summary/>
-       <CreateGoal/>
+
+      {summary?.total > 0 ? <Summary/> : <EmptyGoals/>}
+
+      
+
+
+      <CreateGoal />
     </Dialog>
-   
+
   )
 }
 
